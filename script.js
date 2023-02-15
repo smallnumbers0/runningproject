5679741652c1a211192e2997ec584cf2cdb688e5
echo <jchoi53@mail.ccsf.edu>
2023-02-14 20:05:39 -0800

    add light mode

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 document.getElementById('dark').onclick = darkMode
+document.getElementById('light').onclick = lightMode
 
 function darkMode() {
     document.querySelector('body').style.backgroundColor = 'rgb(32, 40, 38)'
     document.querySelector('body').style.color = 'white'
     document.querySelector('.content').style.backgroundColor = 'black'
+}
+
+function lightMode() {
+    document.querySelector('body').style.backgroundColor = 'rgb(117, 188, 170)'
+    document.querySelector('body').style.color = 'black'
+    document.querySelector('.content').style.backgroundColor = 'honeydew'
 }
