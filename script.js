9b38e28c8cbb457237cca1f6830d89b8513cc463
echo <jchoi53@mail.ccsf.edu>
2023-02-15 12:19:00 -0800

    add toggle function for dark mode

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 document.getElementById('dark').onclick = darkMode
-document.getElementById('light').onclick = lightMode
 
 function darkMode() {
-    document.querySelector('body').style.backgroundColor = 'rgb(32, 40, 38)'
-    document.querySelector('body').style.color = 'white'
-    document.querySelector('.content').style.backgroundColor = 'black'
-}
+    document.body.classList.toggle('dark-mode')
+    document.querySelector('.content').classList.toggle('dark-content')
 
-function lightMode() {
-    document.querySelector('body').style.backgroundColor = 'rgb(117, 188, 170)'
-    document.querySelector('body').style.color = 'black'
-    document.querySelector('.content').style.backgroundColor = 'honeydew'
-}
+  }
