536a52c73994be30ad84e69e7471f7c8aad53711
echo <jchoi53@mail.ccsf.edu>
2023-02-14 19:53:10 -0800

    fix header positions

--- script.js
+++ script.js
@@ -4,5 +4,4 @@ function darkMode() {
     document.querySelector('body').style.backgroundColor = 'rgb(32, 40, 38)'
     document.querySelector('body').style.color = 'white'
     document.querySelector('.content').style.backgroundColor = 'black'
-    document.querySelectorAll('.shoe').style.color = 'white'
 }
